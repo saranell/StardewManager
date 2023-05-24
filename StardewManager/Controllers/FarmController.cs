5985497160a324d52069b012b89f138841aeb68a
saranell <76405224+saranell@users.noreply.github.com>
2023-05-24 16:24:04 -0500

    Add project files.

---
+++ StardewManager/Controllers/FarmController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StardewManager.Controllers
+{
+    public class FarmController : Controller
+    {
+        [HttpGet]
+        [Route("/Farm")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
